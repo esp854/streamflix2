239bdf00281063112a45644af40262278ac042f9
Espérant Mbikayi <esperant@example.com>
2025-10-02 14:20:57 +0000

    Connect to Render PostgreSQL

--- server/vite.ts
+++ server/vite.ts
@@ -20,12 +20,12 @@ const cspMiddleware = () => {
         res.setHeader(
           'Content-Security-Policy',
           `default-src 'self'; ` +
-          `script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com https://www.gstatic.com http://localhost:5173 http://127.0.0.1:5000 https://*.paypal.com:* https://*.paypalobjects.com https://*.braintreegateway.com blob:; ` +
-          `script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com https://www.gstatic.com http://localhost:5173 http://127.0.0.1:5000 https://*.paypal.com:* https://*.paypalobjects.com https://*.braintreegateway.com blob:; ` +
+          `script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com https://www.googletagmanager.com https://www.gstatic.com http://localhost:5173 http://127.0.0.1:5000 https://*.paypal.com:* https://*.paypalobjects.com https://*.braintreegateway.com blob:; ` +
+          `script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com https://www.googletagmanager.com https://www.gstatic.com http://localhost:5173 http://127.0.0.1:5000 https://*.paypal.com:* https://*.paypalobjects.com https://*.braintreegateway.com blob:; ` +
           `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com https://translate.googleapis.com; ` +
-          `img-src 'self' data: https:; ` +
+          `img-src 'self' data: https: https://www.google-analytics.com https://www.googletagmanager.com; ` +
           `font-src 'self' data: https://fonts.gstatic.com; ` +
-          `connect-src 'self' https://api.themoviedb.org https://image.tmdb.org https://www.paypal.com https://www.sandbox.paypal.com https://fonts.googleapis.com https://i.pinimg.com https://fonts.gstatic.com; ` +
+          `connect-src 'self' https://api.themoviedb.org https://image.tmdb.org https://www.paypal.com https://www.sandbox.paypal.com https://fonts.googleapis.com https://i.pinimg.com https://fonts.gstatic.com https://www.google-analytics.com https://www.googletagmanager.com; ` +
           `frame-src 'self' https://odysee.com https://player.twitch.tv https://www.youtube.com https://www.youtube-nocookie.com https://player.vimeo.com https://zupload.cc https://zupload.co https://zupload.io https://*.zupload.cc https://*.zupload.co https://*.zupload.io https://www.paypal.com https://www.sandbox.paypal.com; ` +
           `media-src 'self' blob: https:; ` +
           `worker-src 'self' blob:; ` +
